f11f2b77f36f80b5ebd0802b7c91dd5c67cb9cff
fardin-ahmed-shawon <mdshawon7443@gmail.com>
2025-03-13 00:45:31 +0600

    navbar dynamic

--- header.php
+++ header.php
@@ -47,10 +47,10 @@
                         <div class="cart-counter top-cart-counter">0</div>
                     </div>
                     
-                    <div class="txt-area">
+                    <!-- <div class="txt-area">
                         <p class="bold-title">Cart</p>
                         <p class="gray-title"><span class="top-total-price">Tk 0.00</span></p>
-                    </div>
+                    </div> -->
                 </div>
 
                 <div class="login-btn" onclick="window.location.href='registration.php'">
@@ -80,94 +80,46 @@
                 <hr>
                 <ul>
                     <li><a href="index.php">Home</a></li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(1)" class="dropdown-btn">Men's Fashion<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content1">
-                                <a href="#">T-Shirt</a>
-                                <a href="#">Shirt</a>
-                                <a href="#">Jacket</a>
-                                <a href="#">Pant</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(2)" class="dropdown-btn">Women's Fashion<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content2">
-                                <a href="#">Tops</a>
-                                <a href="#">Selowar Kamij</a>
-                                <a href="#">Geans</a>
-                                <a href="#">Bags</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(3)" class="dropdown-btn">Kids<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content3">
-                                <a href="#">Kids item 1</a>
-                                <a href="#">Kids item 2</a>
-                                <a href="#">Kids item 3</a>
-                                <a href="#">Kids item 4</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(4)" class="dropdown-btn">Gadgets & Electronics<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content4">
-                                <a href="#">Gadgets item 1</a>
-                                <a href="#">Gadgets item 2</a>
-                                <a href="#">Gadgetss item 3</a>
-                                <a href="#">Gadgets item 4</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(5)" class="dropdown-btn">Cosmetics<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content5">
-                                <a href="#">Cosmetics item 1</a>
-                                <a href="#">Cosmetics item 2</a>
-                                <a href="#">Cosmetics item 3</a>
-                                <a href="#">Cosmetics item 4</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(6)" class="dropdown-btn">Health & Beauty<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content6">
-                                <a href="#">Beauty item 1</a>
-                                <a href="#">Beauty item 2</a>
-                                <a href="#">Beauty item 3</a>
-                                <a href="#">Beauty item 4</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(7)" class="dropdown-btn">Home & Living<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content7">
-                                <a href="#">Home item 1</a>
-                                <a href="#">Home item 2</a>
-                                <a href="#">Home item 3</a>
-                                <a href="#">Home item 4</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div class="dropdown">
-                            <button onclick="dropdownBtn(8)" class="dropdown-btn">Kitchen<i class="ri-arrow-down-s-line"></i></button>
-                            <div class="content content8">
-                                <a href="#">Kitchen item 1</a>
-                                <a href="#">Kitchen item 2</a>
-                                <a href="#">Kitchen item 3</a>
-                                <a href="#">Kitchen item 4</a>
-                            </div>
-                        </div>
-                    </li>
+                    <?php
+                        include 'database/dbConnection.php';
+
+                        // Fetch main categories
+                        $mainCategoriesSql = "SELECT * FROM main_category";
+                        $mainCategoriesResult = $conn->query($mainCategoriesSql);
+
+                        if ($mainCategoriesResult->num_rows > 0) {
+                            $index = 0;
+                            while ($mainCategory = $mainCategoriesResult->fetch_assoc()) {
+                                $index++;
+                                $mainCtgName = $mainCategory['main_ctg_name'];
+
+                                // Prepare statement for subcategories
+                                $subCategoriesStmt = $conn->prepare("SELECT * FROM sub_category WHERE main_ctg_name = ?");
+                                $subCategoriesStmt->bind_param("s", $mainCtgName);
+                                $subCategoriesStmt->execute();
+                                $subCategoriesResult = $subCategoriesStmt->get_result();
+                                ?>
+                                <li>
+                                    <div class="dropdown">
+                                        <button onclick="dropdownBtn(<?php echo $index; ?>)" class="dropdown-btn"><?php echo $mainCtgName; ?><i class="ri-arrow-down-s-line"></i></button>
+                                        <div class="content content<?php echo $index; ?>">
+                                            <?php
+                                            if ($subCategoriesResult->num_rows > 0) {
+                                                while ($subCategory = $subCategoriesResult->fetch_assoc()) {
+                                                    echo '<a href="#">' . $subCategory['sub_ctg_name'] . '</a>';
+                                                }
+                                            } else {
+                                                echo '<a href="#">No Items Found!</a>';
+                                            }
+                                            ?>
+                                        </div>
+                                    </div>
+                                </li>
+                                <?php
+                                $subCategoriesStmt->close();
+                            }
+                        }
+                    ?>
                 </ul>
             </div>
         </div>
